a9bbc175a33e2a4fc78b9af861dc9c0c5abd9fe0
Lavrenti DeLavrenti <one.lasha@gmail.com>
2025-12-13 11:41:31 -0800

    chore: convert all Terraform modules to placeholders

--- nedlia-IaC/modules/cognito/main.tf
+++ nedlia-IaC/modules/cognito/main.tf
@@ -16,7 +16,7 @@ variable "environment" {
 #   value       = aws_cognito_user_pool_client.main.client_id
 #   description = "Cognito User Pool Client ID"
 # }
-}
+#}
 
 output "user_pool_id" {
   value = aws_cognito_user_pool.main.id
